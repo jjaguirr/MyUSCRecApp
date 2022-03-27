3f0db0d84ea5ce66d917caeed96facb4f5223837
David Schroeder <dfschroe@usc.edu>
2022-03-26 17:18:41 -0700

    Summary Page

--- app/src/main/java/com/caffeinatedfingers/myuscrecapp/MainActivity.java
+++ app/src/main/java/com/caffeinatedfingers/myuscrecapp/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void onButtonClicked(View view){
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
