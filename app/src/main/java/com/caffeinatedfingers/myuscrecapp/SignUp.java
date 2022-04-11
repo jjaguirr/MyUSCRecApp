5cae36fe8238720943b88ad55b5e16861233d1b5
Chloe Hagmann <chagmann@usc.edu>
2022-04-11 11:54:02 -0700

    chloe: user data stored in firestore cloud database

--- app/src/main/java/com/caffeinatedfingers/myuscrecapp/SignUp.java
+++ app/src/main/java/com/caffeinatedfingers/myuscrecapp/SignUp.java
@@ -1,26 +1,31 @@
 package com.caffeinatedfingers.myuscrecapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class SignUp extends AppCompatActivity
 {
-    EditText uFullName, uEmail, uPassword, uUserId;
+    EditText uFullName, uEmail, uPassword, uUscId;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    //firebase user id not usc id
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,12 +34,13 @@ public class SignUp extends AppCompatActivity
 
         setContentView(R.layout.activity_sign_up);
         uFullName = findViewById(R.id.fullName);
-        uUserId = findViewById(R.id.user_ID);
+        uUscId = findViewById(R.id.user_ID);
         uEmail = findViewById(R.id.email);
         uPassword = findViewById(R.id.password);
 
         Button signUp = findViewById(R.id.btn_sign_up);
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         //check if the user is logged in --> never logged out
         if(fAuth.getCurrentUser() != null)
@@ -43,52 +49,62 @@ public class SignUp extends AppCompatActivity
             finish();
         }
 
-        signUp.setOnClickListener(new View.OnClickListener()
-        {
+        signUp.setOnClickListener(view -> {
+            String email = uEmail.getText().toString().trim();
+            String password = uPassword.getText().toString().trim();
+            String fullName = uFullName.getText().toString().trim();
+            String uscID = uUscId.getText().toString();
 
-            @Override
-            public void onClick(View view) {
-                String email = uEmail.getText().toString().trim();
-                String password = uPassword.getText().toString().trim();
+            // validate the data in email and password - check for empty fields and such
+            if(TextUtils.isEmpty(email))
+            {
+                uEmail.setError("Email is Required");
+                return;
+            }
 
-                // validate the data in email and password - check for empty fields and such
-                if(TextUtils.isEmpty(email))
+            if(TextUtils.isEmpty(password))
+            {
+                uPassword.setError("Password is Required");
+                return;
+            }
+
+            if(password.length() < 6)
+            {
+                uPassword.setError("Password must be at least 6 characters");
+            }
+
+            //register user in firebase
+            fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+                if(task.isSuccessful())
                 {
-                    uEmail.setError("Email is Required");
-                    return;
-                }
+                    Toast.makeText(SignUp.this, "Account Created, Welcome!", Toast.LENGTH_SHORT).show();
+                    userID = fAuth.getCurrentUser().getUid();
+                    DocumentReference documentReference = fStore.collection("users").document(userID);
+                    //store the data in the document --> hash map
+                    Map<String, Object> user = new HashMap<String, Object>();
+                    // insert data into map using put
+                    user.put("fName", fullName);
+                    user.put("email", email);
+                    user.put("uscID", uscID);
 
-                if(TextUtils.isEmpty(password))
-                {
-                    uPassword.setError("Password is Required");
-                    return;
-                }
-
-                if(password.length() < 6)
-                {
-                    uPassword.setError("Password must be at least 6 characters");
-                }
-
-                //register user in firebase
-                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful())
-                        {
-                            Toast.makeText(SignUp.this, "Account Created, Welcome!", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(SignUp.this, MapActivity.class));
+                    // now insert into cloud database
+                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>(){
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d("TAG", "onSuccess: user Profile is created for" + userID);
                         }
+                    });
+                    startActivity(new Intent(SignUp.this, MapActivity.class));
+                }
 
-                        else
-                        {
-                            Toast.makeText(SignUp.this, "Error creating account!" + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
-
-                        }
-                    }
-                });
+                else
                 {
+                    Toast.makeText(SignUp.this, "Error creating account!" + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
 
                 }
+            });
+            {
+
             }
         });
 
